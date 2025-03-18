2adbd18ab83624b9c9c53ef7f4e854eba8349f79
eliasheininger <eliasheininger@web.de>
2025-03-18 14:06:02 +0700

    added fonts and changed width and css stuff

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,12 @@ function App() {
 
   return (
     <div className="app">
-      <h1 className="center fs-30">React Invoice Generator</h1>
+      <h1 className="center fs-45">GET PAID WITHOUT PAYING</h1>
+      <p className="center ">
+        No sign-ups. No complicated dashboards. Just edit your invoice right here, right now and
+        download instantly. It’s free, open-source, and built for freelancers & small agencies who
+        want less hassle and more time for their actual work.
+      </p>
       <InvoicePage data={data} onChange={onInvoiceUpdated} />
     </div>
   )
